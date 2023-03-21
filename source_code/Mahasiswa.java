5c06e525fd75a781c9754e94ab7579d3225ce997
riskaaa0911 <119839421+riskaaa0911@users.noreply.github.com>
2023-03-21 16:30:28 +0700

    Add files via upload

---
+++ source_code/Mahasiswa.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*Saya Riska Nurohmah [2109103] mengerjakan Tugas Praktikum 5 dalam mata kuliah Desain dan Pemrograman Berorientasi Objek
+untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
+package daftarmahasiswa;
+
+/**
+ *
+ * @author sekar
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String gender;
+    
+    public Mahasiswa(String nim, String nama, String nilai, String gender){
+        this.nim = nim;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.gender = gender;
+    }
+    
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public String getNim() {
+        return this.nim;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public String getNilai() {
+        return this.nilai;
+    }
+    
+    public String getGender() {
+        return this.gender;
+    }
+}
